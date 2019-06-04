951e8c0c00eaa523c544accaf58559624d148d17
Megan Nepshinsky <apple@apples-mbp-89.home>
2019-06-04 05:23:36 -0500

    Initial commit

---
+++ LoopsChallenge.playground/Contents.swift
@@ -0,0 +1,9 @@
+var names = ["Abby", "Caleah", "Mia", "Anna Liner", "Amy", "Cassie", "Hanna", "Samantha", "Savannah", "Chloe", "Rosy", "Jessica", "Nataleigh", "Sophie", "Karina", "Gabriella", "Christine", "Scarlett", "Alyssa", "Fatema", "Nasya", "Anna Taylor", "Anna Tetreault"]
+
+var  hobbies = [String]()
+
+hobbies.append("Basketball")
+
+hobbies.append(contentsOf: ["Bowling", "Golf", "Ballet"])
+
+print(hobbies)
